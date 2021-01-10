8cfc49e84a7b90e643b8544069cd47d32a4212da
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-10 04:05:44 -0500

    refactor

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 var testTeam = "hawks";
 var teamPlayers = "";
-var teamArray = [];
 var list = $("#data");
-var playerId = 203992;
 var teamStatsArray = [];
 
+//buttons for testing
 $("#button").click(function () {
-  let url = "https://cors-anywhere.herokuapp.com/http://data.nba.net/10s/prod/v1/2020/teams/" + testTeam + "/roster.json";
-
-  $.getJSON(url, function (result) {
-    $.each(result.league.standard.players, function (i, field) {
-      $("#data").append(field.personId.toString() + " ");
-      teamArray.push(field.personId);
-    });
-    for (let i = 0; i < teamArray.length; i++) {
-      getPlayerInfo(teamArray[i]);
-    }
-    console.log(teamArray);
-  });
+  buildTeam(testTeam);
 });
 
 $("#button2").click(function () {
   getPlayerNames(teamArray);
 })
 
+//console logs array of player codes as strings
+function buildTeam(team) {
+  var teamArray = [];
+  let url = "https://cors-anywhere.herokuapp.com/http://data.nba.net/10s/prod/v1/2020/teams/" + team + "/roster.json";
+  $.getJSON(url, function (result) {
+    $.each(result.league.standard.players, function (i, field) {
+      teamArray.push(field.personId);
+    });
+  });
+  console.log(teamArray);
+  let url2 = "https://cors-anywhere.herokuapp.com/http://data.nba.net/10s/prod/v1/2020/players.json";
+  $.getJSON(url2, function (result) {
+    var plrs = result.league.standard;
+    for (let i = 0; i < teamArray.length; i++) {
+      var plr = plrs.filter(player => player.personId === teamArray[i].toString());
+      var nameStr = plr[0].firstName + " " + plr[0].lastName;
+      console.log(nameStr);
+    }
+  })
+
+}
+
+
 function getPlayerInfo(playerId) {
   let url = "https://cors-anywhere.herokuapp.com/http://data.nba.net/10s/prod/v1/2020/players/" + playerId + "_profile.json";
   $.getJSON(url, function (result) {
     var yearStats = result.league.standard.stats.latest;
-    //console.log(result)
-    //console.log(yearStats.min);
-    //console.log(yearStats.plusMinus);
+    console.log(yearStats.min);
+    console.log(yearStats.plusMinus);
   });
 }
 
